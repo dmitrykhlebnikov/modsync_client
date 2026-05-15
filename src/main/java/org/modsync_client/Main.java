121f610288d9ab85f9d69f5c9c690ef5c73f8293
dmitrykh <dmitry.norge@gmail.com>
2026-05-15 17:26:39 +0200

    feat: wire Main to print manifest URL and resolved paths

--- src/main/java/org/modsync_client/Main.java
+++ src/main/java/org/modsync_client/Main.java
@@ -1,17 +1,30 @@
 package org.modsync_client;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] argv) {
+        Args args;
+        try {
+            args = Args.parse(argv);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+            return;
         }
+
+        java.nio.file.Path minecraftDir;
+        java.nio.file.Path stateFile;
+        try {
+            minecraftDir = Paths.resolveMinecraftDir(args);
+            stateFile    = Paths.resolveStateFile();
+        } catch (IllegalStateException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+            return;
+        }
+
+        System.out.println("Manifest URL : " + args.manifestUrl());
+        System.out.println("Minecraft dir: " + minecraftDir);
+        System.out.println("State file   : " + stateFile);
     }
 }
